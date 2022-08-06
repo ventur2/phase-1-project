ac0101457a95361e09e731127429b5cda1a3d7c5
rachell <dazzlemevanity@gmail.com>
2022-08-05 22:30:38 -0400

    trying to work through CSS error

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    
+})
+
+function getRecipes() {
+    fetch("http://localhost:3000/recipes")
+        .then(response => response.json())
+        .then(response => response.forEach(recipe => {
+            holdRecipes(recipe)
+            hoveredRecipe()
+        }
+    ))
+}
+  
+getRecipes()
+
+
+function holdRecipes(recipe){ 
+    
+    const recipeContainer = document.querySelector("#recipe-container")
+    const div = document.createElement("div")
+    div.classList.add("recipe-card")
+    const h2 = document.createElement("h2")
+    h2.classList.add("pClass")
+    h2.innerText = recipe.name
+    const p = document.createElement("p")
+    p.innerText = recipe.description
+    p.classList.add("pDescription")
+    const img = document.createElement("img")
+    img.src = recipe.image
+    img.classList.add("imgClass")
+    const bttn = document.createElement("button")  
+    bttn.classList.add("bttnClass")
+    bttn.id = recipe.id
+    bttn.textContent = "Like!"
+    bttn.addEventListener("click", () => {
+        alert("You just liked the recipe!")
+    })
+    div.append(h2,img,bttn,p)
+    recipeContainer.append(div)
+
+
+}
+function hoveredRecipe () {
+    document.querySelectorAll(".recipe-card").forEach( hovered => {
+        hovered.addEventListener("mouseenter", (e) => {
+            const hovering = document.querySelector(".pDescription")
+            // console.log(hovered)
+            hovering.classList.add(".display")
+        })
+        hovered.addEventListener("mouseleave", (e) => {
+            // console.log("mouseoff",e)
+    
+            const hovering = hovered.querySelector(".pDescription")
+            hovering.classList.remove(".display")
+    })    
+    
+    })
+}
+
+
+
+// document.querySelector('.recipe-card').addEventListener("mouseenter", entering);
