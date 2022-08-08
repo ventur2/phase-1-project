405c2516ea9fa25068d5bcf6ca2e65a5dbad2768
rachell <dazzlemevanity@gmail.com>
2022-08-08 00:57:34 -0400

    add second function

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ function getRecipes() {
     ))
 }
 getRecipes()
+function holdRecipes(recipe){ 
+    
+    const recipeContainer = document.querySelector("#recipe-container")
+    const div = document.createElement("div")
+    div.classList.add("recipe-card" + recipe.id)
+    const h2 = document.createElement("h2")
+    h2.classList.add("pClass")
+    h2.innerText = recipe.name
+    const p = document.createElement("p")
+    p.innerText = recipe.description
+    p.classList.add("pDescription" + recipe.id)
+    p.classList.add("hide")
+    const img = document.createElement("img")
+    img.src = recipe.image
+    img.classList.add("imgClass")
+    const bttn = document.createElement("button")  
+    bttn.classList.add("bttnClass")
+    bttn.id = recipe.id
+    bttn.textContent = "Like!"
+    bttn.addEventListener("click", () => {
+        alert("You just liked the recipe!")
+    })
+    div.append(h2,img,bttn,p)
+    recipeContainer.append(div)
