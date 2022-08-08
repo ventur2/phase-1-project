7ad5d33e19be185d514fd2782f9545285a256ec0
rachell <dazzlemevanity@gmail.com>
2022-08-08 00:56:27 -0400

    add first function

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     
     
 })
+function getRecipes() {
+    fetch("http://localhost:3000/recipes")
+        .then(response => response.json())
+        .then(response => response.forEach(recipe => {
+            holdRecipes(recipe)
+            hoveredRecipe(recipe)
+        }
+    ))
+}
+    
